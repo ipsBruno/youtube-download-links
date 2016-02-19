e48be19c96902ccb3b7ea658d7af2c35037f4255
ipsBruno <email@brunodasilva.com>
2016-02-19 02:06:36 -0200

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -4,7 +4,8 @@ echo parseYoutube($_GET['id']);
 
 function parseYoutube($data) {  
 
-  $data = 'http://www.youtube.com/get_video_info?video_id='.$data;
+	$data = 'http://www.youtube.com/get_video_info?video_id='.$data;
+
 	parse_str($data, $info);  
 	$streams = explode(',', $info['url_encoded_fmt_stream_map']);  
 	
@@ -31,4 +32,3 @@ function parseYoutube($data) {
  
 
 ?>
-
