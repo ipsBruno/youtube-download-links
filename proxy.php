9f3814f77a79b80a7f4bb7f4ceae001d4a51b772
ipsBruno <email@brunodasilva.com>
2016-02-19 02:05:17 -0200

    Create proxy.php
    PHP proxy for CROSS Browser

---
+++ proxy.php
@@ -0,0 +1,34 @@
+<?php
+
+echo parseYoutube($_GET['id']);
+
+function parseYoutube($data) {  
+
+  $data = 'http://www.youtube.com/get_video_info?video_id='.$data;
+	parse_str($data, $info);  
+	$streams = explode(',', $info['url_encoded_fmt_stream_map']);  
+	
+	$results = [];  
+	
+	foreach($streams as $stream) { 
+		$real_stream = [];  
+
+		parse_str($stream, $real_stream);  
+
+		$real_stream['url'] .= '&signature=' . $real_stream['sig'];  
+	
+	
+  		array_push($results,$real_stream);
+  		
+  	} 
+  	
+  	echo json_encode($results);
+}
+
+
+
+
+ 
+
+?>
+
